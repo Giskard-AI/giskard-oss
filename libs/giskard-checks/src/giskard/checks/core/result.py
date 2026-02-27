f79f1b9e3c3915a92b951216763069a88ee45778
nightcityblade <jackchen@haloailabs.com>
2026-02-28 00:14:51 +0800

    chore: drop `from __future__ import annotations` from text_matching.py

--- libs/giskard-checks/src/giskard/checks/core/result.py
+++ libs/giskard-checks/src/giskard/checks/core/result.py
@@ -209,7 +209,7 @@ class ScenarioResult[InputType, OutputType](BaseModel):
     """
 
     scenario_name: str = Field(..., description="Scenario name")
-    steps: list[TestCaseResult]  # TODO: rename to test_cases
+    steps: list["TestCaseResult"]  # TODO: rename to test_cases
     duration_ms: int = Field(..., description="Total execution time in milliseconds")
     final_trace: Trace[InputType, OutputType] = Field(
         ..., description="Final trace state after execution"
